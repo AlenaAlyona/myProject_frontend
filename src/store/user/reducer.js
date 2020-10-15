651910f00ac3c6b31d688fb2e4a123488c7a42ca
AlenaAlyona <izakson91@gmail.com>
2020-10-15 20:15:53 +0200

    created slice for user in store

---
+++ src/store/user/reducer.js
@@ -0,0 +1,34 @@
+import {
+  LOG_OUT,
+  LOGIN_SUCCESS,
+  TOKEN_STILL_VALID,
+  USERS_FETCHED,
+} from "./actions";
+
+const initialState = {
+  token: localStorage.getItem("token"),
+  user: {},
+  children: [],
+  allUsers: [],
+};
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case LOGIN_SUCCESS:
+      localStorage.setItem("token", action.payload.token);
+      return { ...state, user: action.payload };
+
+    case LOG_OUT:
+      localStorage.removeItem("token");
+      return { ...initialState, token: null };
+
+    case TOKEN_STILL_VALID:
+      return { ...state, ...action.payload };
+
+    case USERS_FETCHED:
+      return { ...state, allUsers: action.payload };
+
+    default:
+      return state;
+  }
+};
