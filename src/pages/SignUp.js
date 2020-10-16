47f653dc855a22f3bea907f32927e1d6557edf31
AlenaAlyona <izakson91@gmail.com>
2020-10-16 10:31:44 +0200

    made a set up for sign up and log in page

---
+++ src/pages/SignUp.js
@@ -0,0 +1,7 @@
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Button, Jumbotron } from "react-bootstrap";
+
+export default function SignUp() {
+  return <div>Hello from Sign Up Page</div>;
+}
