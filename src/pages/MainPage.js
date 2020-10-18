5b10b81193496de5d2946c7d27283c1cd99170a0
AlenaAlyona <izakson91@gmail.com>
2020-10-18 12:43:55 +0200

    made main page boilerplate

---
+++ src/pages/MainPage.js
@@ -0,0 +1,8 @@
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { Button, Jumbotron } from "react-bootstrap";
+
+export default function MainPage() {
+  return <div>Hello from Main Page</div>;
+}
