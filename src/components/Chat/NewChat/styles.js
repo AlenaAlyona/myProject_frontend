84686ece635791901473644ed6ecc9606ecacd8b
AlenaAlyona <izakson91@gmail.com>
2020-10-20 22:53:02 +0200

    displayed new chat form on a main page

--- src/components/Chat/NewChat/styles.js
+++ src/components/Chat/NewChat/styles.js
@@ -2,13 +2,13 @@ const styles = (theme) => ({
   main: {
     width: "auto",
     display: "block", // Fix IE 11 issue.
-    marginLeft: theme.spacing() * 3,
-    marginRight: theme.spacing() * 3,
-    [theme.breakpoints.up(400 + theme.spacing() * 3 * 2)]: {
-      width: 400,
-      marginLeft: "auto",
-      marginRight: "auto",
-    },
+    // marginLeft: theme.spacing() * 3,
+    // marginRight: theme.spacing() * 3,
+    // [theme.breakpoints.up(400 + theme.spacing() * 3 * 2)]: {
+    //   width: 400,
+    //   marginLeft: "auto",
+    //   marginRight: "auto",
+    // },
   },
   paper: {
     padding: `${theme.spacing() * 2}px ${theme.spacing() * 3}px ${
